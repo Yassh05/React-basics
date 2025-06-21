7b4d7f8f91c492ad229c3e2b3fbafb0501d5d2e6
Yassh05 <yashsharmaa555@gmail.com>
2025-06-21 22:49:09 +0530

    react basics

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} ur web name</p>
+        </footer>
+    );
+}
+export default Footer
---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+function Header(){
+        return (
+            <header>
+                <h1>My web</h1>
+                <nav>
+                    <ul>
+                        <li> <a href="">1</a></li>
+                        <li> <a href="">2</a></li>
+                        <li> <a href="">3</a></li>
+                    </ul>
+                </nav>
+                <hr />
+            </header>
+        );
+}
+export default Header
